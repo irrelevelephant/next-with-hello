dba57f84ee92c4508a54dc5e5f0e038b48e96f68
rohanharikr <rohanharikumar80@gmail.com>
2022-11-08 16:32:09 +0400

    fix: update btn cdn endpoint

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link href="https://cdn.hello.coop/css/hello-button.css" rel="stylesheet" />
+                <link href="https://cdn.hello.coop/css/hello-btn.css" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
